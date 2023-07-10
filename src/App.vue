4679669ef13e13509598f014868153a4be355f5d
Sherrie <SherryBirkin@users.noreply.github.com>
2023-07-10 09:30:02 +0000

    feat(local: linechart): 尝试折线图雏形

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import { accountLogin } from '@/api/account/login';
-import GloMethods from '@/utils/gloMethods';
+// import { onMounted } from 'vue';
+// import { accountLogin } from '@/api/account/login';
+// import GloMethods from '@/utils/gloMethods';
 
-onMounted(async () => {
-  const { data } = await GloMethods.commonReqHandler(accountLogin, {
-    username: 'henry',
-    password: '123',
-  });
+// onMounted(async () => {
+//   const { data } = await GloMethods.commonReqHandler(accountLogin, {
+//     username: 'henry',
+//     password: '123',
+//   });
 
-  console.log(data);
-});
+//   console.log(data);
+// });
 </script>
 
 <template>
--- src/views/Charts/LineChart.vue
+++ src/views/Charts/LineChart.vue
@@ -1,7 +1,109 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import * as echarts from 'echarts';
+import { onMounted, onUnmounted, computed } from 'vue';
+
+const myEcharts = echarts;
+const dayCount = ref(0);
+const xAxisNameList = computed(() => Array(dayCount).fill('').map((item, ind) => ind + 1));
+const getTargetDayCount = (targetMonth) => {
+  const date = new Date();
+
+  date.setMonth(targetMonth + 1);
+  date.setDate(0);
+  dayCount.value = date.getDate();
+};
+const initChart = () => {
+  const targetElem = document.querySelector('.chart-container');
+  const chart = myEcharts.init(targetElem);
+
+  chart.setOption({
+    tooltip: {
+      trigger: 'item',
+      formatter: (params) => {
+        const { dataIndex, data } = { ...params };
+
+        return `${dataIndex}: ${data}`;
+      },
+      grid: {
+        containLabel: true,
+        top: '8px',
+        right: 0,
+        bottom: 0,
+        left: 0,
+      },
+      xAxis: {
+        type: 'category',
+        data: xAxisNameList,
+        axisLabel: {
+          interval: 0,
+          rotate: 30,
+          fontSize: 12,
+        },
+        axisLine: {
+          show: true,
+          lineStyle: {
+            // color: this.axisColor,
+            width: 1,
+          }
+        },
+      },
+      yAxis: {
+        type: 'value',
+        minInterval: 1,
+        min: 0,
+        max: null,
+        axisLine: {
+          show: true,
+          lineStyle: {
+            // color: this.axisColor,
+            width: 1,
+          }
+        },
+        splitLine: {
+          show: true,
+          lineStyle: {
+            color: this.splitLineColor,
+          }
+        },
+        splitNumber: 5,
+      },
+      series: [
+        {
+          data: [],
+          type: 'line',
+          smooth: true,
+
+          // 显示数值
+          itemStyle: {
+            normal: {
+            label: {
+              show: true,
+            //   color: this.valColor,
+            },
+          },
+        },
+        // areaStyle,
+        },
+      ],
+    //   color: this.lineColorList,
+    },
+  });
+};
+
+onMounted(() => {
+  getTargetDayCount(new Date().getMonth());
+  initChart();
+});
+onUnmounted(() => {
+    debugger;
+  myEcharts.dispose();
+});
+</script>
 
 <template>
-  <div class="line-chart">折线图</div>
+  <div class="line-chart">
+    <div class="chart-container"></div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
