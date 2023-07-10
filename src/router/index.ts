2bbe31f5c409f378b04377965af47a43bf3feee8
Sherrie <SherryBirkin@users.noreply.github.com>
2023-07-10 02:41:35 +0000

    Update 2 files

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,62 +1,80 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import JumpView from '@/components/JumpView.vue';
-import type { RouteRecord } from 'vue-router';
+import { createRouter, createWebHistory } from "vue-router";
+import JumpView from "@/components/JumpView.vue";
+import type { RouteRecord } from "vue-router";
 
 export const routes: RouteRecord[] = [
   {
-    path: '/',
-    redirect: { name: 'workBench' },
+    path: "/",
+    redirect: { name: "workBench" },
   },
   {
-    path: '/login',
-    name: 'login',
-    component: () => import('@/views/Login/index.vue'),
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/Login/index.vue"),
   },
   {
-    path: '/main',
-    name: 'main',
-    component: () => import('@/views/Main/index.vue'),
+    path: "/main",
+    name: "main",
+    component: () => import("@/views/Main/index.vue"),
     children: [
       {
-        path: '/home',
-        name: 'home',
+        path: "/home",
+        name: "home",
         component: JumpView,
         meta: {
-          title: '首页',
+          title: "首页",
         },
         children: [
           {
-            path: 'work-bench',
-            name: 'workBench',
-            component: () => import('@/views/Home/WorkBench.vue'),
+            path: "work-bench",
+            name: "workBench",
+            component: () => import("@/views/Home/WorkBench.vue"),
             meta: {
-              title: '工作台',
+              title: "工作台",
             },
           },
           {
-            path: 'overview',
-            name: 'overview',
-            component: () => import('@/views/Home/Overview.vue'),
+            path: "overview",
+            name: "overview",
+            component: () => import("@/views/Home/Overview.vue"),
             meta: {
-              title: '概览',
+              title: "概览",
             },
           },
         ],
       },
       {
-        path: '/test',
-        name: 'test',
+        path: "/test",
+        name: "test",
         component: JumpView,
         meta: {
-          title: '测试',
+          title: "测试",
         },
         children: [
           {
-            path: 'auto-test',
-            name: 'autoTest',
-            component: () => import('@/views/Test/AutoTest.vue'),
+            path: "auto-test",
+            name: "autoTest",
+            component: () => import("@/views/Test/AutoTest.vue"),
             meta: {
-              title: '自动化测试',
+              title: "自动化测试",
+            },
+          },
+        ],
+      },
+      {
+        path: "/charts",
+        name: "charts",
+        component: JumpView,
+        meta: {
+          title: "图表",
+        },
+        children: [
+          {
+            path: "line-chart",
+            name: "lineChart",
+            component: () => import("@/views/Charts/LineChart.vue"),
+            meta: {
+              title: "折线图",
             },
           },
         ],
@@ -69,4 +87,4 @@ const router = createRouter({
   routes,
 });
 
-export default router; 
+export default router;
---
+++ src/views/Charts/LineChart.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="line-chart">折线图</div>
+</template>
+
+<style lang="scss" scoped>
+.line-chart {
+}
+</style>
